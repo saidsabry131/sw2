b3503211ffb302c045755a1683c873fa65feed5a
saidsabry131 <saidsabry131@gmail.com>
2024-05-05 00:29:15 +0300

    remove regidter

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -1,5 +1,5 @@
 <?php
-/*
+
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
@@ -15,33 +15,32 @@ class RegisterController extends Controller
     use RegistersUsers;
 
     
-    protected $redirectTo = '/home';
+    // protected $redirectTo = '/home';
 
     
-    public function __construct()
-    {
-        $this->middleware('guest');
-    }
+    // public function __construct()
+    // {
+    //     $this->middleware('guest');
+    // }
 
-    protected function validator(array $data)
-    {
-        return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
-            'user_type' => 'required|string|in:admin,doctor,student', 
-        ]);
-    }
+    // protected function validator(array $data)
+    // {
+    //     return Validator::make($data, [
+    //         'name' => ['required', 'string', 'max:255'],
+    //         'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+    //         'password' => ['required', 'string', 'min:8', 'confirmed'],
+    //         'user_type' => 'required|string|in:admin,doctor,student', 
+    //     ]);
+    // }
 
     
-    protected function create(array $data)
-    {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
-            'user_type'=>$data['user_type']
-        ]);
-    }
+    // protected function create(array $data)
+    // {
+    //     return User::create([
+    //         'name' => $data['name'],
+    //         'email' => $data['email'],
+    //         'password' => Hash::make($data['password']),
+    //         'user_type'=>$data['user_type']
+    //     ]);
+    // }
 }
-*/
